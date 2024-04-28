d7fcc929b9ddd0500bb55cf21ff1224ecae60b50
Jimmy Wong <cy.jimmy.wong@gmail.com>
2024-04-28 18:17:14 +0800

    add missing annotation

--- src/main/java/com/cyjimmy/springnote/repository/NoteRepository.java
+++ src/main/java/com/cyjimmy/springnote/repository/NoteRepository.java
@@ -2,7 +2,9 @@ package com.cyjimmy.springnote.repository;
 
 import com.cyjimmy.springnote.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NoteRepository extends JpaRepository<Note, Integer> {
 
 }
