bf624700fbb3d28016745937b91942bf16fcdd51
Jimmy Wong <cy.jimmy.wong@gmail.com>
2024-04-28 18:17:29 +0800

    change url

--- src/main/java/com/cyjimmy/springnote/controller/NoteController.java
+++ src/main/java/com/cyjimmy/springnote/controller/NoteController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/note")
 public class NoteController {
     @Autowired
     private NoteService noteService;
