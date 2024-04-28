17b19bb8bf72ce2dca7eb1ff6d6e923ae73a25c5
Jimmy Wong <cy.jimmy.wong@gmail.com>
2024-04-28 17:07:52 +0800

    implements get and post

--- src/main/java/com/cyjimmy/springnote/controller/NoteController.java
+++ src/main/java/com/cyjimmy/springnote/controller/NoteController.java
@@ -1,6 +1,8 @@
 package com.cyjimmy.springnote.controller;
 
 import com.cyjimmy.springnote.model.Note;
+import com.cyjimmy.springnote.service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -8,13 +10,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/")
 public class NoteController {
+    @Autowired
+    private NoteService noteService;
+
     @GetMapping("/getAllNote")
     public List<Note> getAllNote() {
-        return null;
+        return noteService.getAllNotes();
     }
 
     @PostMapping("/addNote")
     public String addNote(@RequestBody Note note) {
+        noteService.addNote(note);
         return "New note added.";
     }
 }
